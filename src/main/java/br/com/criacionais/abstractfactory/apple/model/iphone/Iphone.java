a3806bfb8017c80174f7beee83428f1c078bf374
Daniel Oliveira <danielso2007@gmail.com>
2022-09-13 01:01:49 -0300

    fix(model): corrigir classe Iphone do pacote model (#7)

--- src/main/java/br/com/criacionais/abstractfactory/apple/model/iphone/Iphone.java
+++ src/main/java/br/com/criacionais/abstractfactory/apple/model/iphone/Iphone.java
@@ -3,6 +3,7 @@ package br.com.criacionais.abstractfactory.apple.model.iphone;
 import br.com.criacionais.abstractfactory.apple.factory.abstractfactory.CountryRulesAbstractFactory;
 
 public abstract class Iphone {
+
     CountryRulesAbstractFactory rules;
 
     public Iphone(CountryRulesAbstractFactory rules) {
